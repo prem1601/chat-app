ea1b40eb7094430868b9d9a03e17ec58267d878a
Preetam-chinde-datart <129853671+Preetam-chinde-datart@users.noreply.github.com>
2024-02-21 11:51:28 +0530

    ENV added

--- src/components/Firebase.jsx
+++ src/components/Firebase.jsx
@@ -4,25 +4,25 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.VITE_API_KEY,
-//   authDomain: process.env.VITE_AUTH_DOMAIN,
-//   projectId: process.env.VITE_PROJECT_ID,
-//   storageBucket: process.env.VITE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.VITE_MESSAGE_SENDER_ID,
-//   appId: process.env.VITE_APPID,
-// };
+// console.log('firebase env key', import.meta.env.VITE_API_KEY);
+// console.log('firebase env key', import.meta.env.VITE_AUTH_DOMAIN);
+// console.log('firebase env key', import.meta.env.VITE_PROJECT_ID);
+// console.log('firebase env key', import.meta.env.VITE_STORAGE_BUCKET);
+// console.log('firebase env key', import.meta.env.VITE_MESSAGE_SENDER_ID);
+// console.log('firebase env key', import.meta.env.VITE_APPID);
+// console.log('firebase env key', import.meta.env.VITE_MEASUREMENTID);
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
+};
 
 // Chat app on Temp Mail
-const firebaseConfig = {
-  apiKey: "AIzaSyBeYLm5vr0D6nRbe3D7AwPD-NU0MGia_14",
-  authDomain: "chat-87aca.firebaseapp.com",
-  projectId: "chat-87aca",
-  storageBucket: "chat-87aca.appspot.com",
-  messagingSenderId: "134354364075",
-  appId: "1:134354364075:web:716a96c482bde3227539ee",
-  measurementId: "G-37LQRN9DPY",
-};
+
 
 // initializeApp
 const app = initializeApp(firebaseConfig);
@@ -38,6 +38,5 @@ const storage = getStorage();
 
 // Firestore database
 const db = getFirestore();
-// console.log("firestore firebase db", db);
 
 export { auth, provider, storage, db };
